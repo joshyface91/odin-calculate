a3989902f69784b216f66e3afd9e3ad81ab997f4
Joshua Killpack <46246044+joshyface91@users.noreply.github.com>
2024-06-28 13:55:01 -0600

    Logic For Button to Display Screen I used switch/break in conjunction with addEventListener in order to listen to them simultaneously as inputs are sent to the display screen. I then used if/then checks with the .include method throughout in order to keep the user from duplicating operators or decimals.

--- index.js
+++ index.js
@@ -33,5 +33,149 @@ function operate (operant, valueA, valueB) {
     }
 }
 
+
+
+//populates the display for the calculation on the screen field
+document.addEventListener("click", function showValue(event) {
+    const screen = document.querySelector("#screen");
+
+    switch (event.target.id) {
+        case "1":
+            screen.textContent += "1";
+            break;
+        case "2":
+            screen.textContent += "2";
+            break;
+        case "3":
+            screen.textContent += "3";
+            break;
+        case "4":
+            screen.textContent += "4";
+            break;
+        case "5":
+            screen.textContent += "5";
+            break;
+        case "6":
+            screen.textContent += "6";
+            break;
+        case "7":
+            screen.textContent += "7";
+            break;
+        case "8":
+            screen.textContent += "8";
+            break;
+        case "9":
+            screen.textContent += "9";
+            break;
+        case "0":
+            screen.textContent += "0";
+            break;
+        case ".":
+            let symbolDeci = document.querySelector("#screen").innerHTML;
+            let charsDeci = symbolDeci.split("");
+            let wordDeci = charsDeci.join("");
+            if (charsDeci.includes("+")) {
+                let half = wordDeci.split("+");
+                let halfChars = half[1].split("");
+                if (halfChars.includes(".")) {
+                    screen.textContent = screen.textContent.replace(".", ".");
+                } else {
+                    screen.textContent += ".";
+                }
+            } else if (charsDeci.includes("-")) {
+                let half = wordDeci.split("-");
+                let halfChars = half[1].split("");
+                if (halfChars.includes(".")) {
+                    screen.textContent = screen.textContent.replace(".", ".");
+                } else {
+                    screen.textContent += ".";
+                }
+            } else if (charsDeci.includes("*")) {
+                let half = wordDeci.split("*");
+                let halfChars = half[1].split("");
+                if (halfChars.includes(".")) {
+                    screen.textContent = screen.textContent.replace(".", ".");
+                } else {
+                    screen.textContent += ".";
+                }
+            } else if (charsDeci.includes("/")) {
+                let half = wordDeci.split("/");
+                let halfChars = half[1].split("");
+                if (halfChars.includes(".")) {
+                    screen.textContent = screen.textContent.replace(".", ".");
+                } else {
+                    screen.textContent += ".";
+                }
+            } else if (charsDeci.includes(".")) {
+                    screen.textContent = screen.textContent.replace(".", ".");
+                } else {
+                    screen.textContent += ".";
+                }
+            break;
+        case "+":
+            let symbolPlus = document.querySelector("#screen").innerHTML;
+            let charsPlus = symbolPlus.split("");
+            if (charsPlus.includes("+")) {
+                screen.textContent = screen.textContent.replace("+", "+");
+            } else if (charsPlus.includes("-")) {
+                screen.textContent = screen.textContent.replace("-", "+");
+            } else if (charsPlus.includes("*")) {
+                screen.textContent = screen.textContent.replace("*", "+");
+            } else if (charsPlus.includes("/")) {
+                screen.textContent = screen.textContent.replace("/", "+");
+            } else {
+                screen.textContent += "+";
+            }
+            break;
+        case "-":
+            let symbolMinus = document.querySelector("#screen").innerHTML;
+            let charsMinus = symbolMinus.split("");
+            if (charsMinus.includes("+")) {
+                screen.textContent = screen.textContent.replace("+", "-");
+            } else if (charsMinus.includes("-")) {
+                screen.textContent = screen.textContent.replace("-", "-");
+            } else if (charsMinus.includes("*")) {
+                screen.textContent = screen.textContent.replace("*", "-");
+            } else if (charsMinus.includes("/")) {
+                screen.textContent = screen.textContent.replace("/", "-");
+            } else {
+                screen.textContent += "-";
+            }
+            break;
+        case "*":
+            let symbolMulti = document.querySelector("#screen").innerHTML;
+            let charsMulti = symbolMulti.split("");
+            if (charsMulti.includes("+")) {
+                screen.textContent = screen.textContent.replace("+", "*");
+            } else if (charsMulti.includes("-")) {
+                screen.textContent = screen.textContent.replace("-", "*");
+            } else if (charsMulti.includes("*")) {
+                screen.textContent = screen.textContent.replace("*", "*");
+            } else if (charsMulti.includes("/")) {
+                screen.textContent = screen.textContent.replace("/", "*");
+            } else {
+                screen.textContent += "*";
+            }
+            break;
+        case "/":
+            let symbolDivi = document.querySelector("#screen").innerHTML;
+            let charsDivi = symbolDivi.split("");
+            if (charsDivi.includes("+")) {
+                screen.textContent = screen.textContent.replace("+", "/");
+            } else if (charsDivi.includes("-")) {
+                screen.textContent = screen.textContent.replace("-", "/");
+            } else if (charsDivi.includes("*")) {
+                screen.textContent = screen.textContent.replace("*", "/");
+            } else if (charsDivi.includes("/")) {
+                screen.textContent = screen.textContent.replace("/", "/");
+            } else {
+                screen.textContent += "/";
+            }
+            break;
+    }
+})
+
+//uses "=" button to log data as variables, runs operate() with variables, and displays the result
+
 // testing
 console.log(operate("-", 6.54, 5.37));
